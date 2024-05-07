1a77d94c20974bcee6beea09e67c4c3de4f36728
Nuchit <nuchit@outlook.com>
2024-05-07 17:05:43 +0700

    Add Controller

--- src/main/java/com/janawat/customer/customer/CustomerController.java
+++ src/main/java/com/janawat/customer/customer/CustomerController.java
@@ -1,4 +1,39 @@
 package com.janawat.customer.customer;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/customer")
 public class CustomerController {
+    @Autowired
+    private CustomerService customerService;
+
+    @PostMapping
+    public Customer createCustomer(@RequestBody Customer customer){
+        return customerService.createCustomer(customer);
+    }
+
+    @GetMapping("/{id}")
+    public Customer getCustomerById(@PathVariable Long id){
+        return customerService.getCustomerById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Customer updateCustomer(@PathVariable Long id, @RequestBody Customer customer){
+        return customerService.updateCustomer(id,customer);
+
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCustomer(@PathVariable Long id){
+        customerService.deleteCustomer(id);
+    }
 }
