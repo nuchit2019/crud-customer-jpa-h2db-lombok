4b61765d5a20538de760213a6a1f89eb66ff3711
Nuchit <nuchit@outlook.com>
2024-05-07 16:58:44 +0700

    Implement Customer Service

--- src/main/java/com/janawat/customer/customer/CustomerServiceImpl.java
+++ src/main/java/com/janawat/customer/customer/CustomerServiceImpl.java
@@ -1,4 +1,39 @@
 package com.janawat.customer.customer;
 
-public class CustomerServiceImpl {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Override
+    public Customer createCustomer(Customer customer) {
+       return customerRepository.save(customer);
+    }
+
+    @Override
+    public Customer getCustomerById(Long id) {
+       return customerRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Customer updateCustomer(Long id, Customer customer) {
+       
+        if(customerRepository.existsById(id)){
+            customer.setId(id);
+            return customerRepository.save(customer);
+        }
+
+        return null;
+    }
+
+    @Override
+    public void deleteCustomer(Long id) {
+       
+        customerRepository.deleteById(id);
+    }
+
+
 }
