7b53ef3ed4258dfcbb52c3ea5f50f27314c2452c
Nuchit <nuchit@outlook.com>
2024-05-08 14:56:20 +0700

    Refactor ApiResponse

--- src/main/java/com/janawat/customer/customer/ApiResponse.java
+++ src/main/java/com/janawat/customer/customer/ApiResponse.java
@@ -5,11 +5,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.http.HttpStatus;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Setter
 @Getter
